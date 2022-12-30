5809374077c15eef5ad95e0a7ddad5642d894991
Abhay7l <abhaygupta24803@gmail.com>
2022-12-30 07:56:21 +0530

    created checkout page and checkout cart functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import SignUpForm from "./components/sign-up-form/sign-up-form.component";
 import Shop from "./routes/shop/shop.component";
+import Checkout from "./routes/checkout/checkout.component";
 // const Shop=()=>{
   <Shop></Shop>
 //   return <h1>SHOP</h1>;
@@ -18,7 +19,7 @@ const App=()=>{
     <Route path="/shop" element={<Shop/>}/>
     <Route path="/auth" element={<Authentication/>}/>
     <Route path="/sign-up" element={<SignUpForm/>}/>
-    
+    <Route path="/checkout" element={<Checkout/>}/>
     </Route>
   </Routes>
   );
